dd44990f7e3585415004506d1d3a3eddd5d6af7a
Pedro Henrique Gregório <pedrosgregorio@gmail.com>
2024-06-20 01:10:41 -0300

    Getting url to download

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # poscomp-quiz
+
 Repository created to study and analysis of questions from poscomp
+Requerimentos: bs4
---
+++ generate-questions.py
@@ -0,0 +1,70 @@
+from urllib import request
+import os
+from bs4 import BeautifulSoup
+
+response = request.urlopen("https://www.sbc.org.br/documentos-da-sbc/category/153-provas-e-gabaritos-do-poscomp")
+html = response.read().decode('UTF-8')
+
+
+soup = BeautifulSoup(html, "html.parser")
+
+def get_href(tag):
+  return tag.attrs['href']
+
+def verify_class_tag(tag):
+  return "class" not in list(tag.attrs)
+
+def download_pdf(lnk):
+    
+    from selenium import webdriver
+    from time import sleep
+    
+    options = webdriver.ChromeOptions()
+    
+    download_folder = os.getcwd()
+    
+    profile = {"plugins.plugins_list": [{"enabled": False,
+                                         "name": "Chrome PDF Viewer"}],
+               "download.default_directory": download_folder,
+               "download.extensions_to_open": "",
+               "plugins.always_open_pdf_externally": True}
+    
+    options.add_experimental_option("prefs", profile)
+    
+    print("Downloading file from link: {}".format(lnk))
+        
+    driver = webdriver.Chrome(chrome_options = options)
+    driver.get(lnk)
+    
+    filename = lnk.split("/")[4].split(".cfm")[0]
+    print("File: {}".format(filename))
+    
+    print("Status: Download Complete.")
+    print("Folder: {}".format(download_folder))
+    
+    driver.close()
+
+for tag in soup.find_all():
+  if(verify_class_tag(tag)):
+    continue
+  if("jd_categories_title39" in tag.attrs['class']):
+    page_year = request.urlopen("https://www.sbc.org.br" + get_href(tag.contents[0])).read().decode('UTF-8')
+    soup_year = BeautifulSoup(page_year, 'html.parser')
+    links = []
+    for year_tag in soup_year.find_all():
+      if(verify_class_tag(year_tag)):
+        continue
+      if("jd_download_url" in year_tag.attrs['class']):
+        links.append(get_href(year_tag))
+      
+    links = links[::-1]
+    for link in links:
+      page_donwload = request.urlopen("https://www.sbc.org.br" + link).read().decode('UTF-8')
+      soup_download = BeautifulSoup(page_donwload, 'html.parser')
+      for tag_download in soup_download.find_all():
+        if("title" not in list(tag_download.attrs)):
+          continue
+        if("Start downloading" in tag_download.attrs['title']):
+          download_pdf("https://www.sbc.org.br" + get_href(tag_download))
+          print("Feito")
+    
